30e2562c73c41459b2d18bc368d967d849be415a
pranavbs18 <109842852+pranavbs18@users.noreply.github.com>
2026-02-16 16:05:28 +0530

    Assignment 3, Part-1 Complete

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,12 @@
 #include "systemcalls.h"
-
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -9,30 +16,21 @@
 */
 bool do_system(const char *cmd)
 {
+	int ret = system(cmd);
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+	if (ret == -1)
+	{
+		return false;
+	}
 
-    return true;
+	if (WIFEXITED(ret) && WEXITSTATUS(ret) == 0)
+	{
+		return true;
+	}
+
+	return false;
 }
 
-/**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
 
 bool do_exec(int count, ...)
 {
@@ -47,28 +45,40 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    fflush(stdout);
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+	pid_t pid = fork();
 
-    va_end(args);
+	if (pid < 0)
+	{
+		va_end(args);
+		return false;
+	}
 
-    return true;
+	if (pid == 0)
+	{
+		execv(command[0], command);
+		exit(EXIT_FAILURE);
+	}
+
+	int status;
+
+	if (waitpid(pid, &status, 0) < 0)
+	{
+		va_end(args);
+		return false;
+	}
+
+	va_end(args);
+
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+	{
+		return true;
+	}
+
+	return false;
 }
 
-/**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
@@ -82,9 +92,37 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+	fflush(stdout);
 
+	pid_t pid = fork();
 
+	if (pid < 0)
+	{
+		va_end(args);
+		return false;
+	}
+
+	if (pid == 0)
+	{
+		int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd < 0)
+		{
+			exit(EXIT_FAILURE);
+		}
+
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+
+		execv(command[0], command);
+		exit(EXIT_FAILURE);
+}
+int status;
+
+if (waitpid(pid, &status, 0) < 0)
+{
+	va_end(args);
+	return false;
+}
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -92,8 +130,12 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+va_end(args);
 
-    va_end(args);
-
+if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+{
     return true;
+}
+
+return false;
 }
