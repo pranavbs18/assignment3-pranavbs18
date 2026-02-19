21e0dbee76e22e9f2d476938a7fb7d52ded8cc1e
pranavbs18 <109842852+pranavbs18@users.noreply.github.com>
2026-02-19 19:34:37 +0530

    Assignment 4 part-1 complete

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,17 +10,66 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* thread_func_args =
+        (struct thread_data *) thread_param;
+
+    // Wait before obtaining mutex
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    // Lock mutex
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // Hold mutex
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    // Unlock mutex
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
+
+    return thread_param;
+}
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
-}
-
+  
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
+  
+{
+    struct thread_data *data =
+        (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    if (!data)
+        return false;
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+
+    if (rc != 0)
+    {
+        free(data);
+        return false;
+    }
+
+    return true;
+}
+
+  /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
      *
